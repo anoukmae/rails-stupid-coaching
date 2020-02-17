68ce8f2885a07d8a7b438c8d8000e1b8f7e6cfd3
Anouk Escher <anouk.escher@rub.de>
2020-02-17 14:56:42 +0100

    2nd ex stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "questions#ask" # allererste Seite, auf der ich lande, wenn ich localhost öffne
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
 end
